ae65d628e70c732c35a9a5ff02294647bfb6bd16
赵祥兵 <zhaoxiangbing@scrmtech.com>
2023-09-20 17:30:26 +0800

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,18 +10,26 @@ set -e
 pnpm docs:build
 
 # 进入待发布的 dist/ 目录
-cd docs/.vitepress/dist
+
+# cd docs/.vitepress
+
+source_dir="docs/.vitepress/dist"
+destination_dir="docs/dist"
+
+cp -r "$source_dir" "$destination_dir"
+
+
 
 # 提交打包静态网站到 gh-pages 分支
-git init
-git add .
-git commit -m 'deploy'
-git push origin gh-pages
+# git init
+# git add .
+# git commit -m 'deploy'
+# git push origin gh-pages
 # 部署到 https://<username>.github.io/<repo>
-# git push -f git@github.com:zhaoxuesong0526/smart-design-vue.git main:gh-pages
+# git push -f git@github.com:zhaoxuesong0526/smart-design-js.git master:gh-pages
 
 # 提交所有代码到github
 # cd ../../../
-# git add .
-# git commit -m 'update'
-# git push
+git add .
+git commit -m 'update'
+git push
